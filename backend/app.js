84d6982e77084c5a5245005cbe22e5b11f3fdd10
OlgaKG <flory_oli@mail.ru>
2023-08-16 02:20:34 +0200

    readme

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,12 @@ app.use(cors({
   maxAge: 30,
 }));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routes);
 
 app.use(errorLogger);
